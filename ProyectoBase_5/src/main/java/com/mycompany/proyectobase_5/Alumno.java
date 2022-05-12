970a1ba5c5b2d1d8c885c7a786123ac640ad1710
fejESCOM <104636965+fejESCOM@users.noreply.github.com>
2022-05-12 08:31:08 -0500

    jhjk

--- ProyectoBase_5/src/main/java/com/mycompany/proyectobase_5/Alumno.java
+++ ProyectoBase_5/src/main/java/com/mycompany/proyectobase_5/Alumno.java
@@ -4,10 +4,48 @@
  */
 package com.mycompany.proyectobase_5;
 
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  *
  * @author escom
  */
-public class Alumno {
+@Entity
+@Table 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Alumno implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    @Column(name = "nombreAlumno", length =50, nullable=false)
+    private String nombreAlumno;
+
+    @Column(name = "paternoAlumno", length =50, nullable=false)
+    private String paternoAlumno;
+    
+    @Column(name = "maternoAlumno", length =50, nullable=false)
+    private String maternoAlumno; 
+    
+    @Column(name = "emailAlumno", length = 100, nullable=false)
+    private String emailAlumno;
+    
+    @Temporal(TemporalType.DATE)
+    @Column(name = "fechaCreacion", nullable=false)
+    private Date fechaCreacion; 
     
 }
