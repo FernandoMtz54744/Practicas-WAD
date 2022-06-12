43678f954cb54803675471da87b5f6184ffc690d
Fernando <fer_f@outlook.com>
2022-06-12 14:20:27 -0500

    Entidades creadas

---
+++ ProyectoFinal/src/main/java/com/ipn/mx/modelo/entidades/Comentario.java
@@ -0,0 +1,38 @@
+package com.ipn.mx.modelo.entidades;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Cortes Lopez Jaime Alejandro
+ * @author Godinez Montero Esmeralda
+ * @author Martinez Martinez Fernando
+ */
+
+@Entity
+@Table 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Comentario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idComentario")
+    private int idComentario;
+    
+    @Column(name = "comentario", length =100, nullable=false)
+    private String comentario;
+    
+    @Column(name = "idUsuario", nullable=false)
+    private int idUsuario;
+ 
+    @Column(name = "idPlatillo", nullable=false)
+    private String idPlatillo;
+}
